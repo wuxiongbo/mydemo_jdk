23e8d02056667e2d6e378758687ee33c2eb613b2
吴雄博 <wuxiongbo@mycs.cn>
2021-06-17 18:53:27 +0800

    Spring 工具类

--- src/main/java/spring/SpringDemo.java
+++ src/main/java/spring/SpringDemo.java
@@ -15,7 +15,7 @@ import java.lang.reflect.Field;
  */
 public class SpringDemo {
     public static void main(String[] args){
-
+        // 反射获取属性
         Field userNameField = ReflectionUtils.findField(UserInfo.class, "userName", String.class);
         userNameField.setAccessible(true);
 
@@ -27,6 +27,7 @@ public class SpringDemo {
                 .setAge(18)
                 .setEmailAddress("1111@ww.com");
 
+        // 获取属性的值
         String userName= (String)ReflectionUtils.getField(userNameField, user);
 
         System.out.println(userName);
